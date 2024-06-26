8b0f4fe8834299ca25121b5a8c8ca123df10b3c2
Steve Khuu <stephen.khuu.88@gmail.com>
2024-06-26 15:56:35 -0400

    Fix src bug
    🖕

--- src/config/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { SERVER_URL } from "../constants";
-
-const instance = axios.create({ baseURL: SERVER_URL });
-
-export default instance;
--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const SERVER_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://mythicals.onrender.com"
-    : "http://localhost:8000";
